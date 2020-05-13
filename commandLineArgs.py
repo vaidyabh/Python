aeb970e3748044f5ee9a98b0f498d53c58991b18
Abhinav Vaidya <abhinavv1@birlasoft.com>
2020-05-13 21:39:13 +0530

    Adding commandLineArg file

---
+++ commandLineArgs.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Passing command line arguments')
+parser.add_argument('-f', '--file', help = 'File name')
+parser.add_argument('-c', '--country', help = 'Country name')
+
+args = parser.parse_args()
+
+file = args.file
+country = args.country
+
+print('file is', file)
+print('country is', country)
